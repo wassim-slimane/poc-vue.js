8cf5c1922d3e6605d42357c4a6eb96967cb8f0f1
wassim-slimane <wassim.slimane01@gmail.com>
2022-06-20 15:43:19 +0200

    function de déconnexion

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <script setup>
 import { RouterView } from "vue-router";
+import { onMounted, ref } from 'vue'
+import HomePage from './views/HomePage.vue'
 
+const connected = ref(false);
+
+onMounted(() => {
+  if(localStorage.user) {
+    connected.value = true;
+  }
+})
+
+function logout() {
+  connected.value = false;
+  if (localStorage.user) {
+    localStorage.removeItem('user');
+  }
+}
 </script>
 
 <template>
-  <RouterView />
+  <button v-if="connected === true" @click="logout">Se déconnecter</button>
+  <RouterView>
+  </RouterView>
 </template>
 
 <style></style>
