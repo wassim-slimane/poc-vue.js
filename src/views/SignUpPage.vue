86708ac63004420ab827a5a6c7f75a2620638641
wassim-slimane <wassim.slimane01@gmail.com>
2022-06-20 14:55:33 +0200

    Appel à l'api pour la création d'un compte

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -1,14 +1,28 @@
 <script setup>
 import { ref } from "vue";
-import { axios } from "axios"
+import axios from "axios"
+import { stringify } from "postcss";
 
 const email = ref(null);
 const password = ref(null);
 
-function onSubmit() {
-    
-    console.log(email.value);
-    console.log(password.value);
+const onSubmit = async() => {
+    console.log("Sending post request...");
+    const response = await fetch("http://localhost:8080/signup", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value
+        })
+    })
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+    }
+    console.log("End request.");
 }
 
 </script>
