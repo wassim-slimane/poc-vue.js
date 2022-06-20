adfb7a08d0c7c844f37f2f845f77f76f1093e583
wassim-slimane <wassim.slimane01@gmail.com>
2022-06-20 14:34:46 +0200

    Création du formulaire

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -1,3 +1,29 @@
+<script setup>
+import { ref } from "vue";
+import { axios } from "axios"
+
+const email = ref(null);
+const password = ref(null);
+
+function onSubmit() {
+    
+    console.log(email.value);
+    console.log(password.value);
+}
+
+</script>
+
 <template>
-    <h1>SignUpPage</h1>
+    <h1>Formulaire d'inscription</h1>
+    <form @submit.prevent="onSubmit">
+        <div>
+            <label for="email">Email :</label>
+            <input type="text" name="email" id="email" v-model="email">
+        </div>
+        <div>
+            <label for="password">Mot de passe :</label>
+            <input type="password" name="password" id="password" v-model="password">
+        </div>
+        <button type="submit">Valider !</button>
+    </form>
 </template>
